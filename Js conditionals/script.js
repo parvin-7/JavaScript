49d5c38b264577c31c6ce04aaefd892a19df63ab
Parvindar Singh <parvindarsingh43958@gmail.com>
2023-12-29 15:48:24 +0530

    Day 2 completed

---
+++ Js conditionals/script.js
@@ -0,0 +1,64 @@
+// console.log("Hello I am Running in the terminal")
+
+// let age = 19
+
+// if (age > 18){
+//     console.log("You can drive")
+// }
+// else{
+//     console.log("You can't drive")
+// }
+
+// let a = 4;
+// let b = 2;
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a/b)
+// console.log(a*b)
+// console.log(a**b)
+// console.log(a%b)
+
+// a = a + 1 //a+=1
+// console.log(a)
+
+// b -=1
+// console.log(b)
+
+// //Ternary operator
+// let x = 7;
+// let z = 9;
+
+// console.log((x>z)?(x-z):(z-x));
+
+//Excercise 2
+//que 1
+let person_age = 13;
+
+if(person_age>10 && person_age<21){
+    console.log("Yes! person's age is between 10 and 20")
+}
+else{
+    console.log("No! person's age is not matching")
+}
+
+//que 3
+
+let number = 6;
+
+if(number%2==0 && number%3==0){
+    console.log("Yes! The number is divisible by both 2 and 3")
+}
+else{
+    console.log("No! Number is not divisible by 2 and 3")
+}
+
+//que 4
+let no = 83;
+
+if(no%2==0 || no%3==0){
+    console.log("Yes! The number is divisible by either 2 or 3")
+}
+else{
+    console.log("No! Number is not divisible by either 2 or 3")
+}
