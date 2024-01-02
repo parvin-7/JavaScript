ae754e6c07b811b0c5500ec3986a8112b373039a
Parvindar Singh <parvindarsingh43958@gmail.com>
2024-01-02 20:58:33 +0530

    Excercise 4

---
+++ color the boxes/script.js
@@ -0,0 +1,7 @@
+let e = document.childNodes[1].childNodes[2].childNodes[1]
+
+e.childNodes[1].style.backgroundColor = "red"
+e.childNodes[3].style.backgroundColor = "yellow"
+e.childNodes[5].style.backgroundColor = "green"
+e.childNodes[7].style.backgroundColor = "blue"
+e.childNodes[9].style.backgroundColor = "purple"
