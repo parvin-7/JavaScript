a7cb3244a5864c2ed88e8616e79dad71e938045e
Parvindar Singh <parvindarsingh43958@gmail.com>
2024-01-05 15:58:43 +0530

    Day 10 completed

---
+++ DOM in Js - 4/script.js
@@ -0,0 +1,52 @@
+// let btn = document.getElementById("btn")
+
+// let bdy = document.body
+
+// btn.addEventListener("click",()=>{
+//     document.body.style.backgroundColor = "grey"
+// })
+
+// btn.addEventListener("dblclick",(e)=>{
+//     document.body.style.backgroundColor = "green"
+//     console.log(e)
+// })
+
+// document.addEventListener("keydown",(e)=>{
+//     console.log(e.key,e.keyCode); //keycode is an deppreciative property
+// })  
+
+// bdy.addEventListener("mouseup",()=>{
+//     document.body.style.backgroundColor = "yellow"
+// })
+// btn.addEventListener("mousedown",()=>{
+//     document.body.style.backgroundColor = "red"
+// })
+
+// btn.addEventListener("contextmenu",(e)=>{
+//     alert("Are u sure to leave this page?")
+//     console.log(e.width);
+// })
+
+
+//Excercise 8
+
+//que 1
+// let button1 = document.querySelector(".alert1");
+// let button2 = document.querySelector(".alert2");
+// let button3 = document.querySelector(".alert3");
+// let button4 = document.querySelector(".alert4");
+
+
+// button1.addEventListener("click",()=>{
+//     alert("You clicked on Button 1!");
+// })
+// button2.addEventListener("click",()=>{
+//     alert("You clicked on Button 2!");
+// })
+// button3.addEventListener("click",()=>{
+//     alert("You clicked on Button 3!");
+// })
+// button4.addEventListener("click",()=>{
+//     alert("You clicked on Button 4!");
+// })
+
